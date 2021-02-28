34c6ff74ee1f41926db57ad08c2bdb2223b501de
juancho20sp <juancho20sp@gmail.com>
2021-02-28 11:23:33 -0500

    Documentation regenerated

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Board"},{"p":"<Unnamed>","l":"BoardTest"},{"p":"<Unnamed>","l":"Canvas"},{"p":"<Unnamed>","l":"Circle"},{"p":"<Unnamed>","l":"Mission"},{"p":"<Unnamed>","l":"MissionTestC2"},{"p":"<Unnamed>","l":"Rectangle"},{"p":"<Unnamed>","l":"Triangle"}]
